58704f2be63e8c9435ee25c1f0de226a2a57c9b6
deckelmouck <nikolaspalm@gmail.com>
2025-07-16 19:03:40 +0200

    update settings page

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,16 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class SettingPage extends StatelessWidget {
-  const SettingPage({Key? key}) : super(key: key);
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  bool isEnglish = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Text('Settings Page'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ListTile(
+                    title: Text('Language'),
+                    subtitle: Text(isEnglish ? 'English' : 'German'),
+                  ),
+                ),
+                Switch(
+                  value: isEnglish,
+                  onChanged: (val) {
+                    setState(() {
+                      isEnglish = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Divider(height: 32),
+            Text('About', style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 8),
+            ListTile(
+              title: Text('Developer'),
+              subtitle: Text('Nikolas Palm'),
+            ),
+            ListTile(
+              title: Text('Support'),
+              subtitle: GestureDetector(
+                onTap: () async {
+                  final Uri emailLaunchUri = Uri(
+                    scheme: 'mailto',
+                    path: 'deckelmouck@gmail.com',
+                  );
+                  await launchUrl(emailLaunchUri);
+                },
+                child: Text(
+                  'deckelmouck@gmail.com',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Homepage'),
+              subtitle: GestureDetector(
+                onTap: () async {
+                  final Uri url = Uri.parse('https://www.deckelmouck.de');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url, mode: LaunchMode.externalApplication);
+                  }
+                },
+                child: Text(
+                  'deckelmouck.de',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Privacy'),
+              subtitle: GestureDetector(
+                onTap: () async {
+                  final Uri url = Uri.parse('https://www.deckelmouck.de/Hoowlib/Privacy');
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url, mode: LaunchMode.externalApplication);
+                  }
+                },
+                child: Text(
+                  'See privacy policy',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Version'),
+              subtitle: Text('0.1.1'),
+            ),
+            ListTile(
+              title: Text('Copyright'),
+              subtitle: Text('© 2025 Nikolas Palm'),
+            ),
+          ],
+        ),
       ),
     );
   }
