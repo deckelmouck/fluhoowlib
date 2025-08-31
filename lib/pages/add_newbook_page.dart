d8569540b0d40dd61d956ee3be0be63e31de91e3
deckelmouck <nikolaspalm@gmail.com>
2025-08-31 16:11:36 +0200

    update modaladdpage ui

--- lib/pages/add_newbook_page.dart
+++ lib/pages/add_newbook_page.dart
@@ -51,123 +51,136 @@ class AddNewbookPageState extends State<AddNewbookPage> {
     final loc = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: AppBar(
-        title: Text(loc.addNewBook),
-        leading: IconButton(icon: Icon(Icons.arrow_back),
-        onPressed: () => Navigator.pop(context),
+        title: Text(
+          loc.addNewBook,
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: loc.bookTitle),
-                onSaved: (value) => _title = value ?? '',
-                validator: (value) => value!.isEmpty ? 'Enter title' : null,
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(labelText: loc.author),
-                onSaved: (value) => _author = value ?? '',
-                validator: (value) => value!.isEmpty ? 'Enter author' : null,
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Text(loc.publicationDate),
-                  Spacer(),
-                  TextButton(
-                          onPressed: () async {
-                            final picked = await showDatePicker(
-                              context: context,
-                              initialDate: _publicationDate ?? DateTime.now(),
-                              firstDate: DateTime(1500),
-                              lastDate: DateTime.now(),
-                            );
-                            if (picked != null) _onPublicationDateChanged(picked);
-                          },
-                          child: Text(_publicationDate != null
-                              ? '${_publicationDate!.day}.${_publicationDate!.month}.${_publicationDate!.year}'
-                              : loc.selectDate),
-                        ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(loc.read),
-                  Spacer(),
-                  Switch(
-                    value: _readed,
-                    onChanged: _onReadedChanged,
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(loc.rating),
-                  Expanded(
-                    child: Slider(
-                      value: _rating,
-                      min: 0,
-                      max: 10,
-                      divisions: 10,
-                      label: _rating.round().toString(),
-                      onChanged: _readed ? _onRatingChanged : null,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), 
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: loc.bookTitle),
+                  onSaved: (value) => _title = value ?? '',
+                  validator: (value) => value!.isEmpty ? 'Enter title' : null,
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  decoration: InputDecoration(labelText: loc.author),
+                  onSaved: (value) => _author = value ?? '',
+                  validator: (value) => value!.isEmpty ? 'Enter author' : null,
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Text(loc.publicationDate),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () async {
+                        final picked = await showDatePicker(
+                          context: context,
+                          initialDate: _publicationDate ?? DateTime.now(),
+                          firstDate: DateTime(1500),
+                          lastDate: DateTime.now(),
+                        );
+                        if (picked != null) _onPublicationDateChanged(picked);
+                      },
+                      child: Text(_publicationDate != null
+                          ? '${_publicationDate!.day}.${_publicationDate!.month}.${_publicationDate!.year}'
+                          : loc.selectDate),
                     ),
-                  ),
-                  Text(_rating.round().toString()),
-                ],
-              ),
-              SizedBox(height: 20),
-              // Finished Date Picker
-              Row(
-                children: [
-                  Text(loc.finishedDate),
-                  Spacer(),
-                  TextButton(
-                    onPressed: _readed
-                        ? () async {
-                            final picked = await showDatePicker(
-                              context: context,
-                              initialDate: _finishedDate ?? DateTime.now(),
-                              firstDate: DateTime(1500),
-                              lastDate: DateTime.now(),
-                            );
-                            if (picked != null) _onFinishedDateChanged(picked);
-                          }
-                        : null,
-                    child: Text(_finishedDate != null
-                        ? '${_finishedDate!.day}.${_finishedDate!.month}.${_finishedDate!.year}'
-                        : loc.selectDate),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    child: Text(loc.cancel),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                  ElevatedButton(
-                    child: Text(loc.save),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        final book = Book(title: _title, author: _author,
-                          readed: _readed, rating: _rating.round(), publicationDate: _publicationDate, finishedDate: _finishedDate);
-                        Navigator.pop(context, book); // Pass book back
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(loc.read),
+                    Spacer(),
+                    Switch(
+                      value: _readed,
+                      onChanged: _onReadedChanged,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(loc.rating),
+                    Expanded(
+                      child: Slider(
+                        value: _rating,
+                        min: 0,
+                        max: 10,
+                        divisions: 10,
+                        label: _rating.round().toString(),
+                        onChanged: _readed ? _onRatingChanged : null,
+                      ),
+                    ),
+                    Text(_rating.round().toString()),
+                  ],
+                ),
+                SizedBox(height: 20),
+                // Finished Date Picker
+                Row(
+                  children: [
+                    Text(loc.finishedDate),
+                    Spacer(),
+                    TextButton(
+                      onPressed: _readed
+                          ? () async {
+                              final picked = await showDatePicker(
+                                context: context,
+                                initialDate: _finishedDate ?? DateTime.now(),
+                                firstDate: DateTime(1500),
+                                lastDate: DateTime.now(),
+                              );
+                              if (picked != null) _onFinishedDateChanged(picked);
+                            }
+                          : null,
+                      child: Text(_finishedDate != null
+                          ? '${_finishedDate!.day}.${_finishedDate!.month}.${_finishedDate!.year}'
+                          : loc.selectDate),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      child: Text(loc.cancel),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    ElevatedButton(
+                      child: Text(loc.save),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          final book = Book(
+                            title: _title,
+                            author: _author,
+                            readed: _readed,
+                            rating: _rating.round(),
+                            publicationDate: _publicationDate,
+                            finishedDate: _finishedDate,
+                          );
+                          Navigator.pop(context, book); // Pass book back
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
